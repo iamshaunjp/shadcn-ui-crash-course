264d6fe47129191df6aee9557fd73b33024d9407
Shaun <shaun@thenetninja.co.uk>
2023-09-25 10:21:40 +0100

    lesson-1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>Recipes for Ninjas</h1>
+          {children}
+        </nav>
+      </body>
     </html>
   )
 }
